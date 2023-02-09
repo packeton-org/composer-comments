7a2cd0309bef8ae3a83df6bcd011d8bc353212e6
Uladzimir Tsykun <tsykun314@gmail.com>
2023-02-09 02:04:16 +0100

    Added GH test workflow

--- tests/run.sh
+++ tests/run.sh
@@ -1,1 +1,15 @@
 #!/bin/bash
+set -ex
+
+curl https://getcomposer.org/composer.phar -o composer.phar
+chmod +x composer.phar
+
+if [[ -f ./phar-patcher ]]; then
+  ./phar-patcher composer.phar
+else
+  ../phar-patcher composer.phar
+fi
+
+./composer.phar dump-autoload
+
+echo "Finished Successfully"
